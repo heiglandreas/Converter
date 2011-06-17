44150a010e1fbcf75ef92b8243f49002c2d85f20
Andreas Heigl <github@heigl.org>
2011-06-17 07:22:15 +0200

    Adds Kilometer-Conversion

---
+++ src/Converter/Length/Kilometer.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * $Id$
+ *
+ * Copyright (c) 2011 Andreas Heigl<andreas@heigl.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the 'Software'), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @category   Converter
+ * @package    Converter
+ * @subpackage Length
+ * @author     Andreas Heigl<a.heigl@wdv.de>
+ * @copyright  2011 Andreas Heigl
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @version    @__VERSION__@
+ * @since      17.06.2011
+ */
+
+namespace Converter\Length;
+
+use \Converter;
+use \Converter\SubjectInterface;
+use \Converter\ObjectInterface;
+
+/**
+ * Convert to and from Kilometers
+ *
+ * @category   Converter
+ * @package    Converter
+ * @subpackage Length
+ * @author     Andreas Heigl<a.heigl@wdv.de>
+ * @copyright  2011- Andreas Heigl
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @version    @__VERSION__@
+ * @since      17.06.2011
+ */
+class Kilometer implements ObjectInterface, SubjectInterface, LengthInterface
+{
+     /**
+     * Convert to the default value
+     *
+     * @param mixed $value
+     *
+     * @return float
+     */
+    public function toDefault ($value)
+    {
+        return (float) $value * 1000;
+    }
+
+    /**
+     * Convert from the default value
+     *
+     * @param mixed $value
+     *
+     * @return float
+     */
+    public function fromDefault($value)
+    {
+        return (float) $value / 1000;
+    }
+}
---
+++ tests/Converter/Length/KilometerTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * $Id$
+ *
+ * Copyright (c) 2011 Andreas Heigl<andreas@heigl.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the 'Software'), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @category   ConverterTests
+ * @package    Converter
+ * @subpackage Length
+ * @author     Andreas Heigl<a.heigl@wdv.de>
+ * @copyright  2011 Andreas Heigl
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @version    @__VERSION__@
+ * @since      17.06.2011
+ */
+
+namespace Tests\Converter\Length;
+
+use \Converter\Length\Kilometer;
+
+/**
+ * Test correct working of the Converter-class
+ *
+ * @category   ConverterTests
+ * @package    Converter
+ * @subpackage Length
+ * @author     Andreas Heigl<a.heigl@wdv.de>
+ * @copyright  2011- Andreas Heigl
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @version    @__VERSION__@
+ * @since      25.03.2011
+ */
+class KilometerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testClassImplementsCorrectInterfaces()
+    {
+        $interfaces = class_implements('\Converter\Length\Kilometer');
+        $this->assertContains('Converter\Length\LengthInterface', $interfaces);
+        $this->assertContains('Converter\TypeInterface', $interfaces);
+        $this->assertContains('Converter\SubjectInterface', $interfaces);
+        $this->assertContains('Converter\ObjectInterface', $interfaces);
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testObjectFunction($input,$output){
+        $obj = new Kilometer();
+        $this->assertEquals($output,$obj->toDefault($input));
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testSubjectFunction($output,$input)
+    {
+        $subj = new Kilometer();
+        $this->assertEquals($output,$subj->fromDefault($input));
+    }
+
+    public function provider()
+    {
+        return array(
+            array(4, 4000.0),
+            array(4.5, 4500.0),
+            array(4.5123, 4512.3),
+            array(0.004, 4.0),
+
+        );
+    }
+}
